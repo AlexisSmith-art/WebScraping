a20f8ddf8754133540bd57ba3f0376219d79f4b5
Jessica Gomez <jgomeznd@gmail.com>
2020-09-19 18:32:13 -0700

    Returning targeted html

--- PatioFurniture/PatioFurniture/Program.cs
+++ PatioFurniture/PatioFurniture/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace PatioFurniture
 {
@@ -10,6 +12,25 @@ namespace PatioFurniture
     {
         static void Main(string[] args)
         {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString("https://losangeles.craigslist.org/search/zip?query=patio+furniture&search_distance=5&postal=90305");
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var nodes = htmlDoc
+                .DocumentNode
+                .Descendants()
+                .Where(node =>
+                    node.Attributes["class"] != null &&
+                    node.Attributes["class"].Value.Contains("result-row"));
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node.InnerHtml);
+            }
+
+            Console.WriteLine(html);
         }
     }
 }
