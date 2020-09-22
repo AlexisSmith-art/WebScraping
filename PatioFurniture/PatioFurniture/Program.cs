97b2691c966f0123c135f200914d0e3f394548ef
Jessica Gomez <jgomeznd@gmail.com>
2020-09-21 21:32:51 -0700

    successfully sends text message and image

--- PatioFurniture/PatioFurniture/Program.cs
+++ PatioFurniture/PatioFurniture/Program.cs
@@ -7,6 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using AngleSharp.Html.Parser;
 using HtmlAgilityPack;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace PatioFurniture
 {
@@ -28,20 +31,34 @@ namespace PatioFurniture
                     var linkToItem = resultRow.QuerySelectorAll(".result-title.hdrlnk").Select(m => m.GetAttribute("href")).FirstOrDefault();
                     var cityOfItem = resultRow.QuerySelector(".result-hood").TextContent;
                     var distanceToItem = resultRow.QuerySelector(".maptag").TextContent;
-                    Console.WriteLine($"Name: {nameOfItem}");
-                    Console.WriteLine($"Link: {linkToItem}");
-                    Console.WriteLine($"City: {cityOfItem}");
-                    Console.WriteLine($"Distance: {distanceToItem}");
 
                     //get images
                     var linkToItemHtml = webClient.DownloadString(linkToItem);
                     parser = new HtmlParser();
                     document = parser.ParseDocument(linkToItemHtml);
                     var itemImg = document.QuerySelectorAll(".swipe-wrap img").Select(m => m.GetAttribute("src")).FirstOrDefault();
-                    Console.WriteLine($"Img: {itemImg}");
+
+                    List<Uri> imgList = new List<Uri>();
+                    Uri uri = new Uri(itemImg);
+                    imgList.Add(uri);
+
+                    string txtMsg = $"{nameOfItem} in {cityOfItem} {distanceToItem} {linkToItem}";
+
+                    string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+                    string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+                    TwilioClient.Init(accountSid, authToken);
+
+                    var to = new PhoneNumber(Environment.GetEnvironmentVariable("MY_PHONE_NUMBER"));
+                    var from = new PhoneNumber(Environment.GetEnvironmentVariable("TWILIO_PHONE_NUMBER"));
+
+                    var message = MessageResource.Create(
+                        to: to,
+                        from: from,
+                        body: txtMsg,
+                        mediaUrl: imgList
+                    );
                 }
             }
-            Console.WriteLine("");
         }
     }
 }
